bb1748d21ea784cc5e92f49f12c50b6ef3234a06
Conorb <x00123839@gmail.com>
2016-10-06 10:06:18 +0100

    Added update of HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,8 @@ public class HelloWorld {
 	public static void main (String[] args){
 	
 		System.out.println("Hello Connor");
+		System.out.println("How are you");
+		System.out.println("Today is Thursday");
 		
 	}
 
